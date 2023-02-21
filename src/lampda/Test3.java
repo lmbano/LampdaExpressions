df647ea457e3dd971fd7d2de036232d409b42890
lmbano <mbanoleon65@gmail.com>
2023-02-21 21:50:45 +0200

    predicates using objects

---
+++ src/lampda/Test3.java
@@ -0,0 +1,40 @@
+package demos;
+
+@FunctionalInterface  //Explicitly making the class a functional intarface
+interface  Cab3 {
+
+    public String bookCab(String source, String destination);
+}
+
+// This code is replaced by the lampda Expression
+
+/*class Ola implements Cab3{
+    public String bookCab(String src, String dest){
+
+        System.out.println(" Ola cab is booksed fro "+ src+ " to " + dest);
+
+        return ("Prise : 50000 Rs");
+    }
+
+    //(s,d)->System.out.println(" Ola cab is booksed fro "+ s+ " to " + d);
+
+
+}///*/
+
+public class Test3 {
+    public static void main(String[] args) {
+      //  Ola ola = new Ola();
+      //  Ola cab = (s,d)->{System.out.println(" Ola cab is booksed fro "+ s+ " to " + d);
+        Cab3 ola =(from , to)->{
+
+            System.out.println(" Ola cab is booksed fro "+ from+ " to " + to);
+
+            return ("Prise : 50000 Rs");
+        };
+
+
+       System.out.println(ola.bookCab("Dzivarasekwa","town"));
+
+
+    }
+}
---
+++ src/predicate/demo2.java
@@ -0,0 +1,4 @@
+package predicate;
+
+public class demo2 {
+}
